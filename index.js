d3fb65189d9ae2f97ddac96ae75507ea902eeff5
Bianchi Emmanuele <bianchi.emmanuele@osdbsesto>
2021-12-09 11:07:11 +0100

    readFile

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var apiServer = express();
+var fs = require("fs");
 
 // All'interno del apiServer è neccessario la porta, l'host e la funzione eseguia quando ascolta 
 
@@ -22,15 +23,37 @@ apiServer.get("/nome", (request, response) => {
     console.log("richiesta get su nome");
     response.send("Ciao il mio nome è "+nome);
 });
-apiServer.get("/mioNome?", (request, response) => {
+apiServer.get("/mioNome", (request, response) => {
     var nome = request.query.nome;
     console.log("richiesta get su nome");
     response.send("Ciao il mio nome è "+nome);
 });
-apiServer.get("/somma?", (request, response) => {
+// https://localhost:3000/Somma?a=1&b=2
+apiServer.get("/somma", (request, response) => {
     // var a = request.query.a;
     // var b = request.query.b;
     // var somma = parseInt(a)+parseInt(b) ;
     console.log("somma request "+ request.query);
     response.send("Somma: "+ (request.query.a - (- request.query.b)));
+});
+
+// https://localhost:3000/studenti?id=1
+apiServer.get("/Studenti", (request, response) => {
+    console.log("studenti id "+ request.query.id);
+    //leggere il file
+    fs.readFile("Studenti.json", (err, data) => {
+        if(err){
+            console.log("error" + err)
+        }else{
+            var studenti = JSON.parse(data);
+            //console.log("studenti:" +  studenti[0].cognome);
+           for(i = 0; i<studenti.length; i++){
+            if(studenti[i].id == request.query.id){
+                console.log("studenti:" +  studenti[i].nome +"  " + studenti[i].cognome);
+                response.send("studente:" +" nome="+  studenti[i].nome +" cognome=" + studenti[i].cognome);
+            }
+           } 
+        }
+
+    });
 });
